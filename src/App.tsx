f465b0a708d73646337f0b93bc636d0daf7f518b
ckx9798 <ckx12@naver.com>
2025-03-27 04:55:42 +0900

    Feat: 프로젝트 세팅

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+/** @format */
+
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="h-screen flex justify-center items-center bg-blue-50">
+      <h1 className="text-2xl font-bold text-blue-500">Tailwind 작동 성공!</h1>
+      <h1 className="text-3xl font-bold text-blue-200">Tailwind 작동 성공!</h1>
+    </div>
+  );
+}
